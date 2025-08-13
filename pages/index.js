2b6714cbf94acd9d37f55e1184bf67eb3b6c8fee
santoshfaltankar7 <santoshfaltankar7@gmail.com>
2025-08-13 15:01:30 +0530

    fixed

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ import MarketOverview from "../components/MarketOverview";
 import LeadsOverview from "../components/LeadsOverview";
 import toast, { Toaster } from "react-hot-toast";
 
+// ✅ Directly import JSON instead of fetching localhost
+import transactionsData from "../public/data/transactions.json";
+
 export default function Home({ data }) {
   const [todos, setTodos] = useState(data.todos);
 
@@ -23,21 +26,18 @@ export default function Home({ data }) {
         <TransactionSnapshot {...data} />
         <Todos todos={todos} onWish={handleWish} onRemind={handleRemind} />
         <AssetsChart data={data.assets} />
-<MarketOverview market={data.market} />
-
-        {/* <LeadsOverview data={data.leads} /> */}
+        <MarketOverview market={data.market} />
       </div>
 
       {/* Full-width LeadsOverview below grid */}
-<div className="mt-4">
-  <LeadsOverview data={data.leads} />
-</div>
+      <div className="mt-4">
+        <LeadsOverview data={data.leads} />
+      </div>
     </Layout>
   );
 }
 
+// ✅ No more localhost fetch
 export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/data/transactions.json");
-  const data = await res.json();
-  return { props: { data } };
+  return { props: { data: transactionsData } };
 }
